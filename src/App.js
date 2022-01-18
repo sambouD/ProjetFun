d252c1066c3b94f9768179a49e7df1e038deffda
sambouD <samboudoucouree@gmail.com>
2022-01-18 10:34:16 +0100

    BaseReact

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Pokemon from "./components/pokemon/Pokemon";
+import "./App.bootstrap.min.css";
+import "./App.css";
+import NavBar from "./components/layout/NavBar";
+import Dashboard from "./components/layout/Dashboard";
+import backgroundImage from "./pattern.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App" style={{ background: `url(${backgroundImage})` }}>
+        <NavBar />
+        <div className="container">
+          <Routes>
+            <Route exact path="/" component={Dashboard} />
+
+            <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+            <Dashboard />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
